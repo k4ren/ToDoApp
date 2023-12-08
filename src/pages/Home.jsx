ff988bf31ff65d3052caae265203d93f519d0bf7
k4ren <kkhakhis2@gmail.com>
2023-12-07 23:55:59 -0300

    prueba componente chip, posible prioridad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Checkbox, Divider, IconButton, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Box, Checkbox, Chip, Divider, IconButton, List, ListItem, ListItemText, Typography } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useState } from "react";
 
@@ -6,9 +6,9 @@ import { useState } from "react";
 export default function Home() {
 
   const [todos, setTodos] = useState([
-    { id: 1, titulo: "Revisar Doc", estado: false, descipcion: 'revisar doc oficial' },
-    { id: 2, titulo: "Iniciar projecto", estado: true, descipcion: 'crear proyecto, revisar componentesbasicos' },
-    { id: 3, titulo: "Crear Repositorio", estado: false, descipcion: 'Crear repositorio pra subir proyecto' },
+    { id: 1, titulo: "Revisar Doc", estado: false, descipcion: 'revisar doc oficial', prioridad:'alta'},
+    { id: 2, titulo: "Iniciar projecto", estado: true, descipcion: 'crear proyecto, revisar componentesbasicos', prioridad:'baja'},
+    { id: 3, titulo: "Crear Repositorio", estado: false, descipcion: 'Crear repositorio pra subir proyecto', prioridad:'media'},
   ]);
 
   const handleToggle = (id) => {
@@ -25,6 +25,19 @@ export default function Home() {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
+  const getPriorityColor = (prioridad) => {
+    switch (prioridad.toLowerCase()) {
+      case "alta":
+        return "#D32F2F";
+      case "media":
+        return "#ed6c02";
+      case "baja":
+        return "#59ABDB";
+      default:
+        return "default";
+    }
+  };
+
   return <>
     <Typography variant="h4" component="h2" sx={{ flexGrow: 1, mb: 2, color: "gray" }}>Lista de Tareas</Typography>
     <Box sx={{flexGrow:1, display:"grid", gap:2}}>
@@ -37,6 +50,10 @@ export default function Home() {
                 onChange={() => handleToggle(todo.id)}
               />
               <ListItemText primary={todo.titulo} secondary={todo.descipcion} style={{ textDecoration: todo.estado ? "line-through" : "none" }} />
+              <Chip
+              label={todo.prioridad}
+              style={{ backgroundColor: getPriorityColor(todo.prioridad), color: "white" }}
+            />
               <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(todo.id)}>
                 <DeleteIcon />
               </IconButton>
